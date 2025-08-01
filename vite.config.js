16cf5b153029b5f6e94fd22cfcd1bf2ec3a20a37
ChainSync Developer <hi@chainsync.store>
2025-08-01 09:13:33 +0100

    refactor: simplify Vite configuration by converting to ES module syntax and improving plugin usage

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const vite_1 = require("vite");
-const plugin_react_1 = __importDefault(require("@vitejs/plugin-react"));
-const path_1 = __importDefault(require("path"));
-exports.default = (0, vite_1.defineConfig)({
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
     root: 'client',
-    plugins: [(0, plugin_react_1.default)()],
+    plugins: [react()],
     build: {
         outDir: '../dist/client',
     },
     resolve: {
         alias: {
-            '@': path_1.default.resolve(__dirname, './client/src'),
-            '@components': path_1.default.resolve(__dirname, './client/src/components'),
-            '@shared': path_1.default.resolve(__dirname, './shared'),
+            '@': path.resolve(__dirname, './client/src'),
+            '@components': path.resolve(__dirname, './client/src/components'),
+            '@shared': path.resolve(__dirname, './shared'),
         },
     },
 });
